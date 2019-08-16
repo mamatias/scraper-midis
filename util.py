b7a2637c0939e33197b4104c9745f69400791336
Matías <matiastorrsrisso@gmail.com>
2019-08-15 22:36:08 -0400

    Cuento del CSV

---
+++ util.py
@@ -0,0 +1,12 @@
+from music21 import midi
+
+def open_midi(midi_path, remove_drums):
+    mf = midi.MidiFile()
+    mf.open(midi_path)
+    mf.read()
+    mf.close()
+    if remove_drums:
+        for i in range(len(mf.tracks)):
+            mf.tracks[i].events = [ev for ev in mf.tracks[i].events if ev.channel != 10]
+    
+    return midi.translate.midiFileToStream(mf)
